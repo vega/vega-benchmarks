188f698d7fc8edc44d619c5c36a2ce8aba084155
Arvind Satyanarayan <arvindsatya1@gmail.com>
2015-07-22 16:27:34 -0400

    Shim Vega views to get processing/rendering times.

--- scripts/runner.js
+++ scripts/runner.js
@@ -26,6 +26,51 @@ var benchmark = (function() {
       s = 0,
       t = null;
 
+  // Shim the vega View components to get times of processing/dataflow vs. rendering.
+  function shimVg1View(view, results) {
+    view._ts = null;
+    if (!view.update._shimmed) {
+      var up = view.update.bind(view);
+      view.update = function(opt) {
+        view._ts = Date.now();
+        return up(opt);
+      };
+
+      var render = view.render.bind(view);
+      view.render = function(items) {
+        log(results, "processing complete", view._ts);
+        view._ts = Date.now();
+        render(items);
+        log(results, "rendering complete", view._ts);
+        return view;
+      };
+    }
+
+    view.update._shimmed = true;    
+  }
+
+  function shimVg2View(view, results) {
+    view._ts = null;
+    if (!view.update._shimmed) {
+      var up = view.update.bind(view);
+      view.update = function(opt) {
+        view._ts = Date.now();
+        return up(opt);
+      };
+
+      var evl = view._renderNode.evaluate.bind(view._renderNode);
+      view._renderNode.evaluate = function(input) {
+        log(results, "dataflow complete", view._ts);
+        view._ts = Date.now();
+        input = evl(input);
+        log(results, "rendering complete", view._ts);
+        return input;
+      };
+    }
+
+    view.update._shimmed = true;
+  }
+
   function log(results, op, t) {
     results.push({
       key: params.lib+" "+op,
@@ -36,7 +81,7 @@ var benchmark = (function() {
     });
   }
 
-  function svg(results, done) {
+  function run(results, done) {
     d3.timer(function() {
       if (s === 0) {
         t = Date.now();
@@ -49,6 +94,13 @@ var benchmark = (function() {
       } else if (s === 2) {
         log(results, "rendered", t);
         t = null;
+        if (window.view) {
+          if (vg.version.match(/1.5/)) {
+            shimVg1View(window.view, results);
+          } else if (vg.version.match(/2.0/)) {
+            shimVg2View(window.view, results);
+          }
+        }
         ++s;
       } else if (s <= R+2) {
         if (t) log(results, op, t);
@@ -64,25 +116,5 @@ var benchmark = (function() {
     });
   }
 
-  function canvas(results, done) {
-    t = Date.now();
-    init(function() {
-      log(results, "parsed", t);
-      t = Date.now();
-      update();
-      log(results, "rendererd", t);
-
-      for (var i = 0; i<R; ++i) {
-        ops[op]();
-        t = Date.now();
-        update();
-        log(results, op, t);
-      }
-
-      return done(results);
-    });
-  }
-
-  // return params.renderer === 'svg' ? svg : canvas;
-  return svg;
+  return run;
 })();
